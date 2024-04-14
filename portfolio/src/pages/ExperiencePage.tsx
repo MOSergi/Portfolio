f27289cabfef7f6a665d421acb7a9c87cc2c39b9
MOSergi <sergioorozmendez@gmail.com>
2024-04-14 08:06:20 +0200

    realizadas algunas modificaciones

--- portfolio/src/pages/ExperiencePage.tsx
+++ portfolio/src/pages/ExperiencePage.tsx
@@ -10,19 +10,19 @@ export const ExperiencePage = ()=>{
     return(
         <div className="flex flex-col mt-10">
             <div className="flex flex-col items-center bg-gradient-to-t from-slate-800 p-10">
-                <h2 className="text-[26px] font-bold border-b-2 border-amber-400">{t("tecnologies")}</h2>
+                <h2 className="text-[26px] font-bold">{t("tecnologies")}</h2>
                 <Tecnologies />
                 <div className="flex flex-col items-center mt-5 text-center">
                     <h2 className="text-[26px] font-bold border-b-2 border-amber-400">{t("OnOtherHand")}</h2>
-                    <h3 className="text-[18px] text-[#B2F1CF] font-bold mt-2 border-b-[1px]">{t("solidPrinciples")}</h3>
-                    <h3 className="text-[18px] text-[#FFBDB3] flex font-bold mt-2 border-b-[1px]">
+                    <h3 className="text-[18px] text-[#B2F1CF] font-bold mt-2">{t("solidPrinciples")}</h3>
+                    <h3 className="text-[18px] text-[#FFBDB3] flex font-bold mt-2">
                         {t("hexagonalArchitecture")}
                         <BsFillHexagonFill size={30}/>
                     </h3>
                 </div>
             </div>
             <div className="flex flex-col items-center mt-10 text-center">
-                <h2 className="text-[26px] font-bold border-b-2 border-amber-400">{t("professionalExperience")}</h2>
+                <h2 className="text-[26px] font-bold">{t("professionalExperience")}</h2>
                 <WorkExperience />
             </div>
         </div>
